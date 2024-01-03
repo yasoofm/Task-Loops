4c03b860e4ebe13148e1bf9fd7f3c6bae6f9e519
Yousef T Y Y A Mandani <83332@kfh.com>
2024-01-03 10:24:19 +0300

    submission

---
+++ Loops.java
@@ -0,0 +1,10 @@
+public class WeekdaysPrinter{
+    public static void main(String[] args) {
+        String[] week = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    
+        for(int i = 1; i < week.length - 1; i++) {
+          System.out.println(week[i]);
+        }
+        
+      }
+}
